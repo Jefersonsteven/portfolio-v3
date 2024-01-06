abd85c161ad1400c2e5496b5c646cfe9e05f7d6c
Jefersonsteven <gfreycode@gmail.com>
2024-01-06 13:04:42 -0500

    Add React integration and update dependencies

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function Navigation() {
+    const [route, setRoute] = useState('home');
+    
+    function handleRouteChange(event: React.MouseEvent<HTMLAnchorElement>) {
+        setRoute(event.currentTarget.hash.slice(1));
+    }
+
+    return (
+        <div className="nav">
+            <div className="nav__interaction">
+              <div className="line"></div>
+              <div className="circles-container">
+                <a onClick={handleRouteChange} href="#home" className={`circle-nav ${route === 'home' ? 'circle-nav--active' : 'circle-nav--disable'}`}></a>
+                <a onClick={handleRouteChange} href="#about" className={`circle-nav ${route === 'about' ? 'circle-nav--active' : 'circle-nav--disable'}`}></a>
+                <a onClick={handleRouteChange} href="#projects" className={`circle-nav ${route === 'projects' ? 'circle-nav--active' : 'circle-nav--disable'}`}></a>
+                <a onClick={handleRouteChange} href="#contact" className={`circle-nav ${route === 'contact' ? 'circle-nav--active' : 'circle-nav--disable'}`}></a>
+              </div>
+            </div>
+            <nav className="nav__options">
+              <a onClick={handleRouteChange} className="option" href="#home">home</a>
+              <a onClick={handleRouteChange} className="option" href="#about">about</a>
+              <a onClick={handleRouteChange} className="option" href="#projects">projects</a>
+              <a onClick={handleRouteChange} className="option" href="#contact">contact</a>
+            </nav>
+        </div>
+    );
+}
